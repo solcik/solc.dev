5454961ed3c802582ac1fc35ed921d70269e0a13
David Šolc <solcik@gmail.com>
2019-03-12 13:45:37 +0100

    next@canary

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /* eslint-disable global-require, no-nested-ternary, import/no-unresolved, import/no-extraneous-dependencies */
-
-const { PHASE_PRODUCTION_SERVER } =
-    process.env.NODE_ENV === 'development'
-        ? {} // We're never in "production server" phase when in development mode
-        : !process.env.NOW_REGION
-        ? require('next/constants') // Get values from `next` package when building locally
-        : require('next-server/constants'); // Get values from `next-server` package when building on now v2
+const { PHASE_PRODUCTION_SERVER } = require('next/constants');
 
 module.exports = phase => {
     if (phase === PHASE_PRODUCTION_SERVER) {
-        // Config used to run in production.
         return {};
     }
 
@@ -32,5 +25,8 @@ module.exports = phase => {
         },
     });
 
-    return withCSS();
+    return {
+        target: 'serverless',
+        ...withCSS(),
+    };
 };
